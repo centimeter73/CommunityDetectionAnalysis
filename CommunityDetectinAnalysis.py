64145bd4639574e115d82efa6e40e8af05ac0656
centimeter73 <97952407+centimeter73@users.noreply.github.com>
2023-04-07 20:52:43 +0800

    Update CommunityDetectinAnalysis.py

--- CommunityDetectinAnalysis.py
+++ CommunityDetectinAnalysis.py
@@ -82,6 +82,7 @@ def flexibilityAnalysis(root_path,DM_number,HC_number):
 		flexibility_all.append(flexibility)
 	return flexibility_all
 
+# 全脑水平灵活性指标统计分析
 def GlobalFlexibilityAnalysis(flexibility_all,DM_number,HC_number):
 	# 全脑水平的flexibility统计分析
 	flex_values_all = [] # 1*120维向量
@@ -128,7 +129,7 @@ def GlobalFlexibilityAnalysis(flexibility_all,DM_number,HC_number):
 			print("--------------")
 			print("两组数据的全脑水平灵活性不存在差异")
 
-
+# 节点水平灵活性指标统计分析
 def NodalFlexibilityAnalysis(flexibility_all,DM_number,HC_number):
 	# 单脑区节点水平的flexibility统计分析
 	p_values = []
@@ -181,11 +182,105 @@ def NodalFlexibilityAnalysis(flexibility_all,DM_number,HC_number):
 		print("--------------")
 		print("两组数据的节点水平灵活性不存在差异")
 
+# 3. 模块忠诚度指标分析
+def moduleAllegianceMatrixAnalysis(root_path,node_number,DM_number,HC_number):
+	module_allegiance_avg = {}
+	subject_id = 0
+	for file in os.listdir(root_path):
+		community_assignment = np.loadtxt(root_path + "/" +file)
+		module_allegiance_matrix_all = {}
+		count = 0
+		subject_id = subject_id + 1
+		for w in range(0,len(community_assignment[0])):
+			count = count + 1
+			module_allegiance_matrix = []
+			for i in range(0,len(community_assignment)):
+				module_allegiance_row = []
+				for j in range(0,len(community_assignment)):
+					if community_assignment[i][w] == community_assignment[j][w]:
+						module_allegiance_value = 1
+					else:
+						module_allegiance_value = 0
+					module_allegiance_row.append(module_allegiance_value)
+				module_allegiance_matrix.append(module_allegiance_row)
+			module_allegiance_matrix_all[str(count)] = module_allegiance_matrix
+
+		# 所有滑动窗下的平均模块忠诚度指标
+		module_allegiance_matrix_avg = []
+		for i in range(0,node_number):
+			module_allegiance_row_avg = []
+			for j in range(0,node_number):
+				values = []
+				for value in module_allegiance_matrix_all.values():
+					values.append(value[i][j])
+				module_allegiance_row_avg.append(np.mean(values))
+			module_allegiance_matrix_avg.append(module_allegiance_row_avg)
+
+		module_allegiance_avg[str(subject_id)] = module_allegiance_matrix_avg
+
+	# 滑动窗平均水平上的模块忠诚度统计分析
+	p_values = []
+	for i in range(0,node_number):
+		for j in range(0,node_number):
+			values = []
+			for value in module_allegiance_avg.values():
+				values.append(value[i][j])
+
+			# 正态性检验
+			norm_flag = False
+			norm_p_value_DM = stats.shapiro(values[0:DM_number])[1]
+			norm_p_value_HC = stats.shapiro(values[DM_number:])[1]
+
+			if norm_p_value_DM > 0.05 and norm_p_value_HC > 0.05:
+				norm_flag = True
+				#print("两组数据均符合正态分布")
+
+			# 方差齐性检验
+			variance_flag = False
+			variance_p_value = stats.levene(values[0:DM_number],values[DM_number:])[1]
+
+			if variance_p_value > 0.05:
+				variance_flag = True
+				#print("两组数据符合方差齐性")
+
+			if norm_flag and variance_flag:
+				print("两组数据符合双样本t检验的前提")
+
+				# 双样本t检验
+				p_value = stats.ttest_ind(values[0:DM_number],values[DM_number:])[1]
+				p_values.append(p_value)
+
+			else:
+				print("数据不符合双样本t检验前提，需重新进行非参数检验")
+				# Mann-Whitney U检验
+				p_value = scipy.stats.mannwhitneyu(values[0:DM_number],values[DM_number:])[1]
+				p_values.append(p_value)
+
+	# bh多重比较校正
+	p_values_corrected = statsmodels.stats.multitest.multipletests(p_values,alpha = 0.05,method = 'fdr_bh')[1]
+
+	flag = False
+	for p_value_corrected in p_values_corrected:
+		if p_value_corrected < 0.05:
+			flag = True
+
+	if flag:
+		print("--------------")
+		print("两组数据的模块忠诚度连接存在差异")
+
+	else:
+		print("--------------")
+		print("两组数据的模块忠诚度连接不存在差异")
+
+
+
 if __name__ == '__main__':
 	root_path = "I:/T2DM/CommunityDetectionAnalysis/246_50TR_1TR_120_0.05_sparsity_results"
 	DM_number = 68
 	HC_number = 52
+	node_number = 246
 	CommunityNumberAnalysis(root_path,DM_number,HC_number) # 社区数量检测
 	flexibility_all = flexibilityAnalysis(root_path,DM_number,HC_number) # 灵活性指标统计分析
 	GlobalFlexibilityAnalysis(flexibility_all,DM_number,HC_number) # 全脑水平的灵活性指标分析
 	NodalFlexibilityAnalysis(flexibility_all,DM_number,HC_number) # 节点水平的灵活性指标分析
+	moduleAllegianceMatrixAnalysis(root_path,node_number,DM_number,HC_number) # 模块忠诚度指标分析
